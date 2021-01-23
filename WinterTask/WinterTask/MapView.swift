094ebb7ce0fa4a06c3a0c997afd12bb55ff70c5a
HakuBill <battleandwar@hotmail.com>
2021-01-23 21:20:25 +0800

    build MapView

--- WinterTask/WinterTask/MapView.swift
+++ WinterTask/WinterTask/MapView.swift
@@ -6,10 +6,19 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    var latitudeIn: Float = 30.67
+    var longitudeIn: Float = 104.07
+    @State var mapDelta = 0.1
+    
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 30.67, longitude: 104.07),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
     var body: some View {
-        Text("Map View")
+        Map(coordinateRegion: $region).edgesIgnoringSafeArea(.all)
     }
 }
 
