9bdc872d6e3a58d14605f4db3ae62a10435d1239
HakuBill <battleandwar@hotmail.com>
2021-02-04 15:19:36 +0800

    add pdf reader

---
+++ WinterTask/WinterTask/FileBrowser/PDFReader.swift
@@ -0,0 +1,21 @@
+//
+//  PDFReader.swift
+//  WinterTask
+//
+//  Created by HakuBill on 2021/02/04.
+//
+
+import SwiftUI
+import PDFKit
+
+struct PDFReader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PDFReader_Previews: PreviewProvider {
+    static var previews: some View {
+        PDFReader()
+    }
+}
