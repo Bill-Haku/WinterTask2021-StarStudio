8dd5f3136a9fab9b35ca6109d6f8511b89b243ab
HakuBill <battleandwar@hotmail.com>
2021-02-19 11:51:48 +0800

    add change profile photo

--- WinterTask/WinterTask/UserInfo/UserInfoDetail.swift
+++ WinterTask/WinterTask/UserInfo/UserInfoDetail.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct UserInfoDetail: View {
     @ObservedObject var userNameIn = userInfo
-    @State var showImagePicker = false
-    @State var image: Image? = nil
+    @State private var isImagePickerDisplay = false
+    @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @State private var selectedImage: UIImage?
     
     var body: some View {
         List {
@@ -18,12 +19,22 @@ struct UserInfoDetail: View {
                 Text("Profile Photo")
                     .font(.title2)
                 Spacer()
-                Image("GirlInside")
-                    .resizable()
-                    .frame(width: 76, height: 80, alignment: .center)
-                    .clipShape(Circle())
-                    .overlay(Circle().stroke(Color.white, lineWidth: 4))
-                    .shadow(radius: 5)
+                if selectedImage != nil {
+                    Image(uiImage: selectedImage!)
+                        .resizable()
+                        .frame(width: 76, height: 80, alignment: .center)
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                        .shadow(radius: 5)
+                }
+                else {
+                    Image("GirlInside")
+                        .resizable()
+                        .frame(width: 76, height: 80, alignment: .center)
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                        .shadow(radius: 5)
+                }
             }
             NavigationLink(destination: UserSetName()) {
                 HStack {
@@ -33,19 +44,56 @@ struct UserInfoDetail: View {
                 }
             }
             Button(action: {
-                
+                self.sourceType = .photoLibrary
+                self.isImagePickerDisplay.toggle()
             }, label: {
-                Text("Change your profile photo")
+                Text("Change your profile photo from photo library")
             })
         }
         .navigationBarTitle(Text("Personal Infomation"), displayMode: .inline)
         .padding()
-
+        .sheet(isPresented: self.$isImagePickerDisplay) {
+            ImagePickerView(selectedImage: self.$selectedImage, sourceType: self.sourceType)
+        }
     }
 }
 
 struct UserInfoDetail_Previews: PreviewProvider {
     static var previews: some View {
         UserInfoDetail()
+    }
+}
+
+//MARK: - UIImagePicker
+struct ImagePickerView: UIViewControllerRepresentable {
+    @Binding var selectedImage: UIImage?
+    @Environment(\.presentationMode) var isPresented
+    var sourceType: UIImagePickerController.SourceType
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = self.sourceType
+        return imagePicker
+    }
+    
+    class Coordinator: NSObject,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
+        var picker: ImagePickerView
+        
+        init(picker: ImagePickerView) {
+            self.picker = picker
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            guard let selectedImage = info[.originalImage] as? UIImage else { return }
+            self.picker.selectedImage = selectedImage
+            self.picker.isPresented.wrappedValue.dismiss()
+        }
+    }
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(picker: self)
     }
 }
